d7d50b995fa93d85b9e0dc6a4827185465cfc35a
tscript3r <alek199202@gmail.com>
2018-12-11 11:52:16 +0100

    Adding ID base entity

--- pet-clinic-data/src/main/java/pl/tscript3r/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package pl.tscript3r.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/pl/tscript3r/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package pl.tscript3r.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
