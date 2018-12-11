e06401b6d68267d1cbbe8816e9939562fd082dae
tscript3r <alek199202@gmail.com>
2018-12-11 13:56:12 +0100

    Implementing Map based services. Closes #6

--- pet-clinic-data/src/main/java/pl/tscript3r/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/model/Pet.java
@@ -31,4 +31,5 @@ public class Pet extends BaseEntity {
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
+
 }
---
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package pl.tscript3r.petclinic.services.map;
+
+import pl.tscript3r.petclinic.model.Pet;
+import pl.tscript3r.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
