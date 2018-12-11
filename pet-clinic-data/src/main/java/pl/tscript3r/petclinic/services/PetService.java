131dd735a29787a58727b9fc29b39288871c4709
tscript3r <alek199202@gmail.com>
2018-12-11 11:06:56 +0100

    Implement Interfaces for Services. Closes #5

---
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package pl.tscript3r.petclinic.services;
+
+import pl.tscript3r.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package pl.tscript3r.petclinic.services;
+
+import pl.tscript3r.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
