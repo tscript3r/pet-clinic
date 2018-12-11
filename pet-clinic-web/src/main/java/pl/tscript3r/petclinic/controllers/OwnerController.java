88f17b60fab0f2c6575ed82fc0d4fefa23485231
tscript3r <alek199202@gmail.com>
2018-12-11 15:43:41 +0100

    Adding owners page and controller. Closes #11

---
+++ pet-clinic-web/src/main/java/pl/tscript3r/petclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package pl.tscript3r.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listVets(){
+        return "owners/index";
+    }
+
+}
