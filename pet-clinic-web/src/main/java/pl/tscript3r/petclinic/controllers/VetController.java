d1ae9846a8f1bd4bf1042e5b1c6e8bdcce672bc9
tscript3r <alek199202@gmail.com>
2018-12-12 15:49:47 +0100

    Adding list of all vets to vets index page. Closes #14

--- pet-clinic-web/src/main/java/pl/tscript3r/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/tscript3r/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package pl.tscript3r.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.tscript3r.petclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
