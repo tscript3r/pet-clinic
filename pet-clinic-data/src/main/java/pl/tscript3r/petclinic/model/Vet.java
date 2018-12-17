7e77c4bc622bac1deb12a9462f97394a3acc3eaa
tscript3r <alek199202@gmail.com>
2018-12-17 14:52:03 +0100

    Added vet speciality, associated with vet entity. Closes #22

--- pet-clinic-data/src/main/java/pl/tscript3r/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/model/Vet.java
@@ -1,6 +1,17 @@
 package pl.tscript3r.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 
 }
