e4663ce7c45543cad6d8a83105b9cc0ede35eee9
tscript3r <alek199202@gmail.com>
2018-12-17 15:18:17 +0100

    Added PetTypes using bootstrap class. Closes #27

--- pet-clinic-web/src/main/java/pl/tscript3r/petclinic/boootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/pl/tscript3r/petclinic/boootstrap/DataInitializer.java
@@ -5,8 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.tscript3r.petclinic.model.Owner;
+import pl.tscript3r.petclinic.model.PetType;
 import pl.tscript3r.petclinic.model.Vet;
 import pl.tscript3r.petclinic.services.OwnerService;
+import pl.tscript3r.petclinic.services.PetTypeService;
 import pl.tscript3r.petclinic.services.VetService;
 
 @Component
@@ -14,11 +16,13 @@ public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
     private final Logger logger = LoggerFactory.getLogger(DataInitializer.class);
 
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
@@ -46,6 +50,16 @@ public class DataInitializer implements CommandLineRunner {
         vetService.save(vet1);
 
         logger.info("Loaded vets..");
+
+        PetType pt0 = new PetType();
+        pt0.setName("Dog");
+        petTypeService.save(pt0);
+
+        PetType pt1 = new PetType();
+        pt0.setName("Cat");
+        petTypeService.save(pt0);
+
+        logger.info("Loaded pet types..");
     }
 
 }
