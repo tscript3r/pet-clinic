761c174e327b9aaaca38fac5f7494cfb6768224d
tscript3r <alek199202@gmail.com>
2018-12-17 15:07:23 +0100

    Implemented petType service map. Closes #24

---
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package pl.tscript3r.petclinic.services;
+
+import pl.tscript3r.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,37 @@
+package pl.tscript3r.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import pl.tscript3r.petclinic.model.PetType;
+import pl.tscript3r.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+}
