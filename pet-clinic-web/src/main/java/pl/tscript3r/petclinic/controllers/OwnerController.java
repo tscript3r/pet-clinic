3f588dab25520fd2686c9691261ad39a3d368a04
tscript3r <alek199202@gmail.com>
2019-01-07 14:39:30 +0100

    Implemented base entity annotations. Closes #30

--- pet-clinic-web/src/main/java/pl/tscript3r/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/tscript3r/petclinic/controllers/OwnerController.java
@@ -14,7 +14,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html", "/owners/find"})
     public String listOwners(Model model){
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
