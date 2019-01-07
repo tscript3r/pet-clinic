a6508289309d958037bf7232d7be09cbc608b181
tscript3r <alek199202@gmail.com>
2019-01-07 15:31:32 +0100

    Created Spring Data JPA Service for Owners. Closes #35

--- pet-clinic-data/src/main/java/pl/tscript3r/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import pl.tscript3r.petclinic.models.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+
 }
