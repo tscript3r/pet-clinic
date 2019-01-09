d9b908b72515cff39c05a53459f5ec4a0eb2bb58
tscript3r <alek199202@gmail.com>
2019-01-09 15:20:40 +0100

    Added spring MVC test for OwnerController. Closes #47

---
+++ pet-clinic-web/src/test/java/pl/tscript3r/petclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,58 @@
+package pl.tscript3r.petclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import pl.tscript3r.petclinic.models.Owner;
+import pl.tscript3r.petclinic.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController ownerController;
+
+    Set<Owner> ownerSet = new HashSet<>();
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        ownerSet = new HashSet<>();
+        ownerSet.add(Owner.builder()
+                .id(1L)
+                .build());
+        ownerSet.add(Owner.builder()
+                .id(2L)
+                .build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(ownerController)
+                .build();
+
+    }
+
+    @Test
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(ownerSet);
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+    }
+}
