bb36360ec093a622bf5f5c3854573d1cae740605
tscript3r <alek199202@gmail.com>
2019-01-17 15:00:56 +0100

    Added ability to add or update a pet. Closes #51

--- pet-clinic-data/src/main/java/pl/tscript3r/petclinic/models/Owner.java
+++ pet-clinic-data/src/main/java/pl/tscript3r/petclinic/models/Owner.java
@@ -16,15 +16,6 @@ import java.util.Set;
 @Table(name = "owners")
 public class Owner extends Person {
 
-    @Builder
-    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
-        super(id, firstName, lastName);
-        this.address = address;
-        this.city = city;
-        this.telephone = telephone;
-        this.pets = pets;
-    }
-
     @Column(name = "address")
     private String address;
 
@@ -37,4 +28,34 @@ public class Owner extends Person {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+
+        if(pets != null) {
+            this.pets = pets;
+        }
+    }
+
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
+    }
 }
