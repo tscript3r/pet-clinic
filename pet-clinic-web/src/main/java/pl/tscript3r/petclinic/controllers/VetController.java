76316c1f813b35b7b8d7e6b404bbcea10d6afb37
tscript3r <alek199202@gmail.com>
2019-01-22 17:12:43 +0100

    Added JSON endpoint for vets. Closes #55

--- pet-clinic-web/src/main/java/pl/tscript3r/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/tscript3r/petclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package pl.tscript3r.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.tscript3r.petclinic.models.Vet;
 import pl.tscript3r.petclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,4 +25,8 @@ public class VetController {
         return "vets/index";
     }
 
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
 }
